ba9ca66a63a594364fcf36ab0cd82d573f84c198
aloosley-ux <a.loosley@gmail.com>
2026-03-14 17:57:14 +0000

    chore: fix logger.d.ts lint errors (replace any with unknown)

--- api/_lib/logger.d.ts
+++ api/_lib/logger.d.ts
@@ -2,9 +2,9 @@
 // This file provides type safety for the logger import in TypeScript files.
 
 declare interface Logger {
-  error: (...args: any[]) => void;
-  warn: (...args: any[]) => void;
-  info: (...args: any[]) => void;
+  error: (...args: unknown[]) => void;
+  warn: (...args: unknown[]) => void;
+  info: (...args: unknown[]) => void;
 }
 
 declare const logger: Logger;
